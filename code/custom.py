d75990c863bba4fe500bfc459fb88a1866a72038
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-02-11 16:50:13 +0000

    feat: add metadata print

--- code/custom.py
+++ code/custom.py
@@ -3,18 +3,20 @@
 
 from picamera2 import Picamera2, Preview
 import time
+import libcamera as lc
 
 picam2 = Picamera2()
 
 # Set camera configuration parameters
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
+config = picam2.create_preview_configuration()
+config = picam2.create_still_configuration()
+picam2.configure(config)
 
 # See section 5.1.2, page 31 (PDF page 32)
 with picam2.controls as controls:
     controls.ExposureTime = 10000
-    controls.AnalogueGain = 1.0
-    # print(controls)
+    # controls.AnalogueGain = 1.0
+    controls.AfMode = lc.controls.AfModeEnum.Continuous
 
 picam2.start_preview(Preview.QTGL)
 picam2.start(show_preview=True)
