4fff1e4244672c40d50b8366185be6a77c9350b0
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-02-16 08:50:24 +0000

    docs: add docstring to CameraDisplay

--- code/legocamera/camera.py
+++ code/legocamera/camera.py
@@ -8,5 +8,10 @@ class LegoCamera:
         pass
 
 class CameraDisplay(Enum):
+    """
+    Defines which window is shown to the user.
+    
+    Either the viewfinder for taking a picture, or gallery for reviewing previous pictures.
+    """
     gallery = Gallery
     viewfinder = Viewfinder
