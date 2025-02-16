aa497410f691e4f43088d64187f3bedf77ae4c25
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-02-16 08:09:34 +0000

    feat: add CameraDisplay enum

--- code/legocamera/camera.py
+++ code/legocamera/camera.py
@@ -1,5 +1,12 @@
 # The core camera itself. The LegoCamera is the class used in the main program.
 
+from displays import Gallery, Viewfinder
+from enum import Enum
+
 class LegoCamera:
     def __init__(self):
         pass
+
+class CameraDisplay(Enum):
+    gallery = Gallery,
+    viewfinder = Viewfinder
