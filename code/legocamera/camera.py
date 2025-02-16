a17d857bfaae1d0fc08b065f4f4946d3837a54f6
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-02-16 11:29:36 +0000

    fix: fix get_camera() and NoCameraException

--- code/legocamera/camera.py
+++ code/legocamera/camera.py
@@ -19,16 +19,14 @@ class CameraDisplay(Enum):
     viewfinder = Viewfinder
 
 class NoCameraException(Exception):
-    def __init__(self, index_error: IndexError):            
+    def __init__(self, index_error: IndexError, from_None: bool = True):            
         # Call the base class constructor with the parameters it needs
         super().__init__('Python did not find a camera attached to this system. Please check you have a camera connected.')
-            
-        # Now for your custom code...
-        self.index_error: IndexError = index_error
 
+        raise self from None if from_None == True else index_error
 
 def get_camera()->Picamera2:
     try:
-        return Picamera2
+        return Picamera2()
     except IndexError as ie:
-        raise NoCameraException(ie)
+        NoCameraException(ie)
