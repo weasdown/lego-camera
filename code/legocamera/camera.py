158b120edf05229317292600a4d76e43918cc152
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-02-16 08:42:53 +0000

    fix: fix widgets import

--- code/legocamera/camera.py
+++ code/legocamera/camera.py
@@ -1,6 +1,6 @@
 # The core camera itself. The LegoCamera is the class used in the main program.
 
-from displays import Gallery, Viewfinder
+from widgets import Gallery, Viewfinder
 from enum import Enum
 
 class LegoCamera:
