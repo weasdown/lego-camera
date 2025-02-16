2fac1165c1bfb5bf96af4522c55b85070d034b12
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-02-16 14:01:27 +0000

    feat: make LegoCamera a subclass of Picamera2, avoid side effect in start_preview()

--- code/legocamera/camera.py
+++ code/legocamera/camera.py
@@ -10,31 +10,21 @@ import time
 from settings import AutofocusSetting, Picam2Configuration
 from widgets import Gallery, Viewfinder
 
-class LegoCamera:
+class LegoCamera(Picamera2):
     def __init__(self, camera_config: Picam2Configuration = Picam2Configuration.still, gallery_path: Path = Path(os.getcwd()), autofocus_setting: AutofocusSetting = AutofocusSetting.single_shot):
         """Creates a camera object with extra settings."""
 
         self.autofocus_setting: AutofocusSetting = autofocus_setting
         self.gallery_path: Path = Path(gallery_path)
 
-        self.picam2: Picamera2 = LegoCamera.get_camera()  # TODO move to start of __init__() (currently lower down only for testing on Pi500)
+        super().__init__()
         print()  # Print a blank line after Picamera2() construction messages
 
         print('Configuring camera...')
-        self._camera_config: dict = camera_config(self.picam2)
+        self.camera_config: dict = camera_config(self)
         print(f'Selected configuration: {self.camera_config}\n')
-        self.picam2.configure(self.camera_config)
+        self.configure(self.camera_config)
         print('Configuration complete!\n')
-
-    @property
-    def camera_config(self) -> dict:
-        return self._camera_config
-
-    @camera_config.setter
-    def camera_config(self, config: dict) -> None:
-        self._camera_config = config
-        print('Reconfiguring with new config')
-        self.picam2.configure(self.camera_config)  # Reconfigure with the new config.
 
     @staticmethod
     def get_camera() -> Picamera2:
@@ -46,17 +36,24 @@ class LegoCamera:
     @property
     def metadata(self) -> dict:
         """Gets PiCamera2's metadata dict."""
-        return self.picam2.capture_metadata()
+        return self.capture_metadata()
 
     def record_video(self) -> None:
         raise NotImplementedError('LegoCamera.record_video() is not yet implemented.')
 
-    def start_preview(self) -> None:
-        self.camera_config = self.picam2.create_preview_configuration()
+    def start_standard_preview(self, duration: float = 3) -> None:
+        """Wrapper for `Picamera2.start_preview()`."""
+        original_config: dict = self.camera_config
+        
+        self.configure(self.create_preview_configuration())
         print('\nStarting preview\n')
-        self.picam2.start_preview(Preview.QTGL)
-        self.picam2.start()
-        time.sleep(5)
+        self.start_preview(Preview.QTGL)
+        self.start()
+        time.sleep(duration)
+        self.stop()
+        self.stop_preview()
+
+        self.configure(original_config)  # Reset to original config to avoid side effect.
 
     def take_picture(self) -> None:
         """Capture a single image."""
@@ -86,11 +83,12 @@ class NoCameraException(Exception):
         raise self from None if from_None == True else index_error
 
 if __name__ == '__main__':
-    lego_cam = LegoCamera()
+    lego_cam = LegoCamera(camera_config=Picam2Configuration.still)
     print(f'Current autofocus setting: {lego_cam.autofocus_setting}')
 
-    lego_cam.start_preview()
+    lego_cam.start_standard_preview()
 
     lego_cam.take_picture()
 
-    print(lego_cam.metadata)
+    print(f'\nMetadata:\n{lego_cam.metadata}')
+    print(f'\nCamera properties:\n{lego_cam.camera_properties}')
